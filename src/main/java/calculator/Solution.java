848bf9e7e1012e9ad22390dfe6b5dfe2126c1230
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-06 08:55:57 +0300

    Remove class RomanNumeral.java

--- src/main/java/calculator/Solution.java
+++ src/main/java/calculator/Solution.java
@@ -35,7 +35,7 @@ public final class Solution {
         return 1 + List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X").indexOf(number);
     }
 
-    static String arabicToRoman(int number) {
+    static String arabicToRoman(final int number) {
         return String.join("",
                 number / 100 == 1 ? "C" : "",
                 new String[]{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}[number % 100 / 10],
